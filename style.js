e75b31de3f1cd06547bf7372d785f6e4575ab9bf
CalamintySerenade <wilsongarate1@gmail.com>
2025-10-29 08:40:49 -0400

    C

--- style.js
+++ style.js
@@ -1,1 +1,10 @@
-document.getElementById("task1").innerText="Complete project documentation";
+document.getElementById("task1").innerText="Complete prohect documentation";
+
+//I declared a variable and added something to print to thr console
+let task=document.getElementById("task2").innerHTML;
+console.log(task+" "+"is selected");
+
+document.getElementById("compl1").addEventListener("click", function(){
+    document.getElementById("task2").style.textDecoration="line-through";
+    document.getElementById("task1").style.textDecoration="line-through";
+});
