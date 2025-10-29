92115a4f4946ad613915d00a68c67dd12535b39a
CalamintySerenade <wilsongarate1@gmail.com>
2025-10-29 08:46:28 -0400

    D

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("taskname1").innerText="Complete prohect documentation";
+
+//I declared a variable and added something to print to thr console
+let task=document.getElementById("taskname2").innerHTML;
+console.log(task+" "+"is selected");
+
+document.getElementById("compl1").addEventListener("click", function(){
+    document.getElementById("taskname2").style.textDecoration="line-through";
+    document.getElementById("taskname1").style.color="red";
+});
