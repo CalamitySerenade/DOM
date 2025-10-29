8b1fceb320186a5f80c4b586339a3e38e0758feb
CalamintySerenade <wilsongarate1@gmail.com>
2025-10-29 09:51:28 -0400

    j

--- script2.js
+++ script2.js
@@ -8,4 +8,13 @@ document.getElementById("addButton").addEventListener("click",function(){
     console.log(origin);
     origin=parseInt(origin)+1;
     document.getElementById("counter").innerHTML=origin;
-})
+});
+
+document.getElementById("multiplier table").addEventListener("click",function(){
+    let origin2=document.getElementById("count").innerHTML;
+    let arr=[];
+    for(let a=1;a<5;a++){
+        arr.push(parseInt(origin2)*(a+1));
+    }
+    document.getElementById(table);
+});
