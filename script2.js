23574ee09e609f82d7c086f6b7f067311e5775ff
CalamintySerenade <wilsongarate1@gmail.com>
2025-10-30 09:00:42 -0400

    k

--- script2.js
+++ script2.js
@@ -18,3 +18,16 @@ document.getElementById("multiplier table").addEventListener("click",function(){
     }
     document.getElementById(table);
 });
+
+//Now, the card has a counter, it is functional, the card made has a functionality
+//Functionality being an increase of 1
+//Both getElementByTag and getElementByClass return an array
+//getElementByTagName works by interacting with tags in HTML
+//Ex: (html,head,body,p,h1,h2,etc)
+//Call with button: Will interact with buttons (array button)
+//Must decide on which one you want to use
+//Similar to tagName method, you have getElementByClass
+//elementsByClassName follows same rules as getElementByTagName
+//by using ClassName, you can target a specific card
+
+document.getElementsByClassName("card")[1].style.backgroundColor="#000";
