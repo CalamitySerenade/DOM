436d4ce2c18e7f17783c792e2a87922d1d3d6d41
CalamintySerenade <wilsongarate1@gmail.com>
2025-11-05 09:51:52 -0500

    dggffh

--- datafile.js
+++ datafile.js
@@ -6,3 +6,27 @@ fetch("https://jsonplaceholder.typicode.com/users")
       container.innerHTML += `<p>${user.name} - ${user.email}</p>`;
     });
   });
+
+  //Task A:
+  fetch("https://jsonplaceholder.typicode.com/posts")
+  .then(res=>res.json())
+  .then(posts=>{
+    const container=document.getElementById("outext");
+    posts.forEach(post=>{
+      container.innerHTML += container.innerHTML += `<p>${post.id} - ${post.title}</p>`
+    })
+  })
+
+//Task B:
+fetch 
+
+
+//Task C:
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(res => res.json())
+.then(users=>{
+  const container=document.getElementById("output");
+  users.forEach(user =>{
+    container.innerHTML += `<p>${user.name} - ${user.address["city"]}</p>`
+  })
+})
